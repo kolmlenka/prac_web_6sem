c5358aa436cbb186dcce1eb760b359e723702aee
kolmlenka <bakhtinaev@my.msu.ru>
2025-04-27 23:24:19 +0300

    added DAO

--- src/main/java/com/example/web/dao/StudentDao.java
+++ src/main/java/com/example/web/dao/StudentDao.java
@@ -1,9 +1,23 @@
 package com.example.web.dao;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import com.example.web.Student;
+
 import java.util.List;
 
 public interface StudentDao extends CommonDAO<Student, Long> {
     List<Student> getAllStudentByGroupId(Long groupId);
     List<Student> getAllStudentByYear(Integer year);
+
+    @Builder
+    @Getter
+    class Filter {
+        private String name;
+    }
+
+    static Filter.FilterBuilder getFilterBuilder() {
+        return Filter.builder();
+    }
 }
