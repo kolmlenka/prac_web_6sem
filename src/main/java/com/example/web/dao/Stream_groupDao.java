3c8d72da4e1794a19932e7180686debfaa8f7466
kolmlenka <bakhtinaev@my.msu.ru>
2025-05-07 17:28:29 +0300

    classes

--- src/main/java/com/example/web/dao/Stream_groupDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.web.dao;
-
-import com.example.web.Stream_group;
-import java.util.List;
-
-public interface Stream_groupDao extends CommonDAO<Stream_group, Long>{
-    Stream_group findStreamByGroupId(Long id);
-}
--- src/main/java/com/example/web/dao/TeacherDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.web.dao;
-
-import com.example.web.Teacher;
-
-public interface TeacherDao extends CommonDAO<Teacher, Long> {
-    Teacher getTeacherNameById(Long teacherId);
-    Teacher getTeacherIdByName(String teacherName );
-}
