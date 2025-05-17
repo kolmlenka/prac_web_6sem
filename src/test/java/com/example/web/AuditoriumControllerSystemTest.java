629b02d4a02647d6a8a0bc2375362b55e179cfe3
kolmlenka <bakhtinaev@my.msu.ru>
2025-05-18 02:17:12 +0300

    system tests added

---
+++ src/test/java/com/example/web/AuditoriumControllerSystemTest.java
@@ -0,0 +1,61 @@
+package com.example.web;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AuditoriumControllerSystemTest {
+
+    private static final String DRIVER_PATH = "/home/kolmlena/chromedriver";
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private final String auditoriumslistTitle = "Список аудиторий";
+    private final String auditoriumTitle = "Аудитория";
+
+
+    @BeforeAll
+    static void setUp() {
+        System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
+    }
+
+    private void login(WebDriver driver) {
+        driver.get(BASE_URL + "/login");
+
+        WebElement usernameField = driver.findElement(By.name("username"));
+        WebElement passwordField = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+
+        usernameField.sendKeys(USERNAME);
+        passwordField.sendKeys(PASSWORD);
+
+        loginButton.click();
+    }
+
+    @Test
+    void AuditoriumsListPage() {
+        WebDriver driver = new ChromeDriver();
+        login(driver);
+        driver.get(BASE_URL + "/auditoriums/list");
+
+        assertEquals(auditoriumslistTitle, driver.getTitle());
+
+        driver.quit();
+    }
+
+    @Test
+    void AuditoriumPage() {
+        WebDriver driver = new ChromeDriver();
+        login(driver);
+        driver.get(BASE_URL + "/auditorium?auditoriumId=1");
+
+        assertEquals(auditoriumTitle, driver.getTitle());
+
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/example/web/CourseControllerSystemTest.java
@@ -0,0 +1,61 @@
+package com.example.web;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CourseControllerSystemTest {
+
+    private static final String DRIVER_PATH = "/home/kolmlena/chromedriver";
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private final String courseslistTitle = "Список курсов";
+    private final String courseTitle = "Основы кибернетики";
+
+
+    @BeforeAll
+    static void setUp() {
+        System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
+    }
+
+    private void login(WebDriver driver) {
+        driver.get(BASE_URL + "/login");
+
+        WebElement usernameField = driver.findElement(By.name("username"));
+        WebElement passwordField = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+
+        usernameField.sendKeys(USERNAME);
+        passwordField.sendKeys(PASSWORD);
+
+        loginButton.click();
+    }
+
+    @Test
+    void CoursesListPage() {
+        WebDriver driver = new ChromeDriver();
+        login(driver);
+        driver.get(BASE_URL + "/courses/list");
+
+        assertEquals(courseslistTitle, driver.getTitle());
+
+        driver.quit();
+    }
+
+    @Test
+    void CoursePage() {
+        WebDriver driver = new ChromeDriver();
+        login(driver);
+        driver.get(BASE_URL + "/course?courseId=7");
+
+        assertEquals(courseTitle, driver.getTitle());
+
+        driver.quit();
+    }
+}
